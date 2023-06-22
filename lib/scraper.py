7d8193b47874744290e29a0e2b4bb8ad24d7eb35
iamkipruto <kelvin.kipruto@student.moringaschool.com>
2023-06-22 13:44:41 +0300

    Web Scrapping

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+
+# We will be uses the our-courses page
+html = requests.get("https://moringaschool.com/our-courses/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-60-black.color-black.mb-20'))
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses: 
+  print(course.contents[0].strip())
